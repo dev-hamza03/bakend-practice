14e0907315889274300670899b36baa0e08f13f8
Hamza <dev.hamza03@gmail.com>
2026-01-25 17:35:18 +0530

    day-3

--- day-3/app.js
+++ day-3/app.js
@@ -2,7 +2,25 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json())
+
+const notes = [];
+
+app.post("/notes", (req, res) => {
+    
+    notes.push(req.body);
+
+    console.log(req.body);
+    
+    res.send("botes created");
+});
+
+app.get("/notes", (req, res) => {
+    res.send(notes);
+});
+
+
 app.listen(3000, () => {
-   console.log("server is running on port 3000")
+    console.log("server is running on port 3000")
 });
 
