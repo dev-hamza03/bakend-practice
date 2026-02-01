092939b2ed60ce22c25c42ae135bfbd85224074d
Hamza <dev.hamza03@gmail.com>
2026-02-01 20:44:40 +0530

    day-6 successfully connected to database

--- day-6/server.js
+++ day-6/server.js
@@ -1,5 +1,17 @@
 const app = require("./src/app");
 
+const mongoose = require("mongoose");
+
+function connectToDb () {
+    mongoose.connect("mongodb+srv://hamza:80an52MYGSJV1c58@cluster0.y4jy881.mongodb.net/")
+    .then(() => {
+        console.log("Connected to database");
+    });
+};
+
+connectToDb();
+
+
 app.listen(3000, () => {
     console.log("server is running or port 3000");
 });
