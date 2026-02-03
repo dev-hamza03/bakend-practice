74fb61145e4474d5ee1f8b55ef147a58f4a27c07
Hamza <dev.hamza03@gmail.com>
2026-02-03 11:26:25 +0530

    created all API's in backend

--- day-9/backend/server.js
+++ day-9/backend/server.js
@@ -1,4 +1,7 @@
 const app = require("./src/app");
+const connectToDb = require("./src/config/database");
+
+connectToDb();
 
 app.listen(3000,() => {
     console.log("serer is running on port 3000");
--- day-9/backend/src/app.js
+++ day-9/backend/src/app.js
@@ -1,8 +1,57 @@
 const express = require("express");
-const connectToDb = require("./config/database");
+const noteModel = require("./models/note.model")
 
 const app = express();
-connectToDb();
+
+app.use(express.json());
+
+/* POST API */
+app.post("/notes", async (req, res) => {
+    const { title, description } = req.body;
+
+    const note = await noteModel.create({
+        title, description
+    });
+
+    res.status(201).json({
+        message: "note created successfully",
+        note
+    });
+});
+
+/* GET Api */
+app.get("/notes", async (req, res) => {
+    const notes = await noteModel.find();
+
+    res.status(200).json({
+        message: "notes fetched successfully",
+        notes
+    });
+});
+
+/* DELETE Api */
+app.delete("/notes/:id", async (req, res) => {
+    const { id } = req.params
+
+    await noteModel.findOneAndDelete(id);
+
+    res.status(204).json({
+        message: "note deleted successfully",
+    });
+});
+
+/* PATCH Api */
+app.patch("/notes/:id", async (req, res) => {
+    const { id } = req.params;
+    const { description } = req.body;
+
+    await noteModel.findByIdAndUpdate(id, { description });
+
+
+    res.status(200).json({
+        message: "note description updated successfully"
+    });
+});
 
 
 module.exports = app;
